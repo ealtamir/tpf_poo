3abeb0eafb0b469b8404f9493c2620880d4429da
Enzo Altamiranda <enzo.alt@gmail.com>
2012-11-03 21:12:47 -0300

    Agregué el código del MenuBar. Aún falta implementar algunos ActionListeners para que funcionen todas las opciones de la misma.

--- src/frontend/StartGame.java
+++ src/frontend/StartGame.java
@@ -2,11 +2,15 @@ package frontend;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.File;
 
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 
 public class StartGame implements ActionListener {
@@ -41,7 +45,8 @@ public class StartGame implements ActionListener {
 						"Silversphere", 
 						game.getBoard().getWidth(),
 						game.getBoard().getHeight(),
-						game
+						game,
+						createMenuBar()
 				);
 			} catch (Exception exception) {
 				if (exception.getMessage() == null) {
@@ -50,7 +55,60 @@ public class StartGame implements ActionListener {
 					JOptionPane.showMessageDialog(gameScreen, exception.getMessage());
 				}
 				exception.printStackTrace();
+				gameScreen.setVisible(true);
 			}
 		}
+	}
+	
+	public JMenuBar createMenuBar() {
+		JMenuBar menuBar = new JMenuBar();
+
+		JMenu file = new JMenu("File");
+		
+		JMenuItem newGame = new JMenuItem("New Game");
+		JMenuItem reset = new JMenuItem("Reset");
+		JMenuItem save = new JMenuItem("Save");
+		JMenuItem close = new JMenuItem("Close");
+		
+		file.add(newGame);
+		file.add(reset);
+		file.add(save);
+		file.add(close);
+		
+		newGame.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// Esto no es estrictamente necesario que lo hagamos.
+				// Acá tengo que darle la oportunidad al jugador de cargar un nuevo mapa.
+				
+			}
+		});
+		reset.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO: Hay que hacer esto para la entrega.
+				// Aca tengo que invocar el método action performed de la clase.
+				
+			}
+		});
+		save.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO: Hay que hacer esto para la entrega.
+				// Acá tengo que serializar el mapa a un archivo que elija el usuario.
+				
+			}
+		});
+		close.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+				
+			}
+		});
+		
+		menuBar.add(file);
+		
+		return menuBar;
 	}
 }
